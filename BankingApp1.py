aeec02a6e2cbc1cd5b281358b84984127302f219
Mpagi Sendawula <103054108+msendawula@users.noreply.github.com>
2022-10-05 01:22:25 -0500

    Removed CSV/Txt file reliance. Fixed MongoDB bug.

--- BankingApp1.py
+++ BankingApp1.py
@@ -1,68 +1,12 @@
-import account
-import csv
 from pymongo import MongoClient
 
-def add_account() -> account.Account:
-    print("Welcome to QuickTeller. Please select an action:")
-    print("\t a) Opening a Savings Account")
-    print("\t b) Opening a Checking Account")
-    typeAccount = input(">>>")
-
-    if not typeAccount == 'a':
-        typeAccount ='b'
-        print("Default Checking Option chosen.")
-
-    print("\n\nEnter Account Holder name (First Name | Last Name):")
-    name = input(">>>")
-
-    print("\n\n Enter new Account Number:")
-    account_number = int(input(">>>"))
-
-    print("\n\n Enter Starting Balance:")
-    balance=str(float(input(">>>")))
-
-    if typeAccount == "a":
-        newAccount = account.Savings(name, account_number, balance)
-    else:
-        newAccount = account.Checking(name, account_number, balance)
-    return newAccount
-
-
-
-def load_accounts():
-    f = open("saved_accounts.txt", "r")
-    lst_accounts = []
-    for line in f:
-        if line ==" ":
-            break
-        account_data = line.split(',')
-        if account_data [3] == "Savings":
-            newAccount = account.Savings(account_data[0], account_data[1], account_data[2])
-        else: 
-            newAccount = account.Checking(account_data[0], account_data[1], account_data[2])
-        lst_accounts.append(newAccount)
-    f.close()
-    return lst_accounts
-
-
-def save_accounts (lst_Accounts):
-    f = open("saved_accounts.txt", "w")
-    for account in lst_Accounts:
-        f.write(account.name + ","+ str(account.account_number) + ","+str(account.balance) +"," + account.account_type + "\n")
-    f.close()
-    import pandas
-    bankaccounts=pandas.read_csv("saved_accounts.txt", header = [0])
-    bankaccounts.to_csv("saved_bankaccounts.csv", index = None)
-                
 
 def main():
     client = MongoClient()
 
     BankAccountsdb = client.BankAccounts
-
-    print("Welcome to the QuickTeller Application")
     
-    lst_Accounts = load_accounts()
+    print("Welcome to the QuickTeller Application".center(75,"="))
 
     while True:
         print("Please select an option:")
@@ -70,22 +14,41 @@ def main():
         print("\tb) View Account")
         print("\tc) Update Account")
         print("\td) Close Account")
-        print("\ts) Save all accounts to MongoDB (Must restart application before saving.)")
         print("\tq) Quit")
         option = input(">>>")
 
         if option == "q":
             break
 
-        elif option == "s":
-            client.drop_database("BankAccounts")
-            for bkacct in lst_Accounts:
-                save_to_db(bkacct, BankAccountsdb)
-
         if option == "a":
-            lst_Accounts.append(add_account())
-            save_accounts(lst_Accounts)     
+#Setting account type
+            print("What type of account does the customer want to open?")
+            print("\t a) Opening a Checking Account")
+            print("\t b) Opening a Savings Account")
+            if input(">>>").lower() == "a":
+                print("You've chosen a Checking Account.")
+                type = "Checking"
+            else:
+                print("You've chosen a Savings Account.")
+                type = "Savings"
 
+#Setting Account Holder Name               
+            print("\n\nEnter Account Holder name (First Name | Last Name):")
+            name = input(">>>")
+
+#Setting Account Number
+            import random
+            print("\n\nAssigning random account number.")
+            account_number = random.randint(25,600)
+
+#Setting up Starting Balance
+            print("\n\n Enter Starting Balance:")
+            balance=float(input(">>>"))
+            newAccountDict = {"Name": name, "Account Number": account_number, "Balance": balance, "Account Type":type}
+            BankAccountsdb.accounts.insert_one(newAccountDict)
+            print(f"The following account information has been inserted into the Database:{newAccountDict}.")
+
+#Allowing for database to be queried by Customer Name or Account Number
         if option == "b":
             print("Please enter Account Holder name or Account Number")
             print("\ta) Account Number")
@@ -94,102 +57,52 @@ def main():
         
             if AccountOption == "a":
                 print("\t Please enter the Account Number")
-                option = input(">>>")
-                with open("saved_bankaccounts.csv") as c:
-                    reader = csv.reader(c)
-                    for row in reader:
-                        if row[1]==option:
-                            print(row)
+                option = int(input(">>>"))
+                x  = BankAccountsdb.accounts.find_one({"Account Number": option},{"_id":0})
+                print(x)
                             
 
             if AccountOption == "b":
                 print("\t Please enter the Account Holder Name")
-                option = input(">>>")                
-                with open("saved_bankaccounts.csv") as c:
-                    reader = csv.reader(c)
-                    for row in reader:
-                        if row[0]==option:
-                            print(row)  
+                option = input(">>>")              
+                x = BankAccountsdb.accounts.find_one({"Name": option},{"_id":0})
+                print(x)
 
+#Code to allow for deposits/withdrawals
         if option == "c":
             print("\tWhat action would you like to perform?")
             print("\ta) Deposit Funds")
             print("\tb) Withdraw Funds")
             BankTransact = input(">>>")
-        
+
 
             if BankTransact == "a":
-                import decimal
                 print("\tPlease enter the Account Number")
-                option = input(">>>")
+                option = int(input(">>>"))
                 print("Please enter the deposit amount.")
-                Deposit = input(">>>")
-                updated_row=[]
-                with open("saved_bankaccounts.csv","r") as d:
-                    csvreader = csv.reader(d)
-                    for row in csvreader:
-                            if option == row[1]:
-                                row[2] = ((float(row[2])) + (float(Deposit)))
-                                updated_row.append(row)
-                            else:
-                                updated_row.append(row)
-                    with open("saved_bankaccounts.csv", "w", newline="") as e:
-                        writer=csv.writer(e)
-                        writer.writerows(updated_row)
-                    import pandas
-                    df=pandas.read_csv("saved_bankaccounts.csv", header = [0])
-                    df.to_csv("saved_accounts.txt", header=False, index = None)
+                option1 = float(input(">>>"))
+#Teller may enter a positive or negative number; Want to make sure that regardless of whether a negative or positive number is entered number entered is added to balance. 
+                Deposit = float(abs(option1))
+                BankAccountsdb.accounts.update_one({"Account Number": option}, {"$inc": {"Balance":Deposit}})
+                
 
 
             if BankTransact == "b":
-                import decimal
                 print("\tPlease enter the Account Number")
-                option = input(">>>")
+                option = int(input(">>>"))
                 print("Please enter the withdrawal amount.")
-                Withdrawal = input(">>>")
-                updated_row=[]
-                with open("saved_bankaccounts.csv","r") as d:
-                    csvreader = csv.reader(d)
-                    for row in csvreader:
-                            if option == row[1]:
-                                row[2] = ((float(row[2])) - (float(Withdrawal)))
-                                updated_row.append(row)
-                            else:
-                                updated_row.append(row)
-                    with open("saved_bankaccounts.csv", "w", newline="") as e:
-                        writer=csv.writer(e)
-                        writer.writerows(updated_row)
-                    import pandas
-                    df=pandas.read_csv("saved_bankaccounts.csv", header = [0])
-                    df.to_csv("saved_accounts.txt", header=False, index = None)     
+                option1 = float(input(">>>"))
+#Teller may enter a positive or negative number; Want to make sure that regardless of whether a negative or positive number is entered number entered is deducted from balance. 
+                Withdrawal = -1*(abs(option1))
+                BankAccountsdb.accounts.update_one({"Account Number": option}, {"$inc": {"Balance":Withdrawal}})
+                  
 
-
+#Code to delete account from database
         if option == "d":
-            print("Please enter the Account Number you wish to delete")
-            option = input(">>>")
-            lines=[]
-            try:
-                with open("saved_accounts.txt", "r") as rf:
-                    lines = rf.readlines()
-                with open("saved_accounts1.txt", "w") as rg:
-                    for line in lines:
-                        if option not in line:
-                            rg.write(line)
-            except:
-                print("Error found. Please try again.")
-
-            with open ("saved_accounts1.txt", "r") as source, open("saved_accounts.txt", "w") as destination:
-                for line in source:
-                    destination.write(line)
-            import pandas
-            bankaccounts=pandas.read_csv("saved_accounts.txt", header = [0])
-            bankaccounts.to_csv("saved_bankaccounts.csv", index = None)
-
-
-def save_to_db(bkacct, BankAccountsdb):
-    dict_acct = {"name": bkacct.name, "account_number": bkacct.account_number, "balance": bkacct.balance, "account_type": bkacct.account_type}
-    BankAccountsdb.accounts.insert_one(dict_acct)
-
+            print("Please enter the Account Number you wish to close.")
+            option = int(input(">>>"))
+            BankAccountsdb.accounts.delete_one({"Account Number": option})
+           
 
 
 if __name__ == "__main__":
--- account.py
+++ account.py
@@ -1,38 +1,17 @@
+#Parent Class
 class Account: 
-    isOpen = True
-
     def __init__(self, name, account_number, balance, account_type):
         self.name = name
         self.account_number=account_number
         self.balance=balance
         self.account_type=account_type
 
-    def __str__(self):
-        return self.name + "," + str(self.account_number) +"," +(self.balance) +"," + str(self.account_type)
-
-# Child class of Account
-
-   
+# Child classes of Account
 class Checking(Account):
     def __init__(self, name, account_number, balance):
-        self.name = name
-        self.account_number=account_number
-        self.balance=balance
         self.account_type = "Checking"
+
 
 class Savings(Account):
     def __init__(self, name, account_number, balance):
-        self.name = name
-        self.account_number=account_number
-        self.balance=balance
         self.account_type = "Savings"
-
-
-def main():
-    a1 = Checking("James Jones", 55855864, 550.65, Checking)
-    c1 = Savings("George Lam", 55856658566, 90005.65, Savings)
-    d1 = Savings("Jesse Lane", 85545755, 905.25, Savings)
-    print(a1)
-
-if __name__ == '__main__':
-    main()
